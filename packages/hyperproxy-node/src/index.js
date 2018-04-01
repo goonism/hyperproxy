4a249e545e1d16f33ab63c407c36754762364f06
Joe Johans <lgvichy@gmail.com>
2018-04-01 01:05:13 -0400

    Hyperproxy node retrive data accomplished

--- packages/hyperproxy-node/src/index.js
+++ packages/hyperproxy-node/src/index.js
@@ -1,12 +1,19 @@
 import Dat from 'dat-node';
 
 // 1. Tell Dat where to download the files
-Dat('/tmp/storage', {
-  // 2. Tell Dat what link I want
-  key: '<dat-key>' // (a 64 character hash from above)
-}, function (err, dat) {
-  if (err) throw err;
+Dat('./tmp/storage', {
+    // 2. Tell Dat what link I want
+    // (a 64 character hash from above)
+    key: '40a7f6b6147ae695bcbcff432f684c7bb5291ea339c28c1755896cdeb80bd2f9/',
+    sparse: true
+}, function(err, dat) {
+    if (err)
+        throw err;
 
-  // 3. Join the network & download (files are automatically downloaded)
-  dat.joinNetwork();
+    // 3. Join the network & download (files are automatically downloaded)
+    dat.joinNetwork();
+
+    dat.archive.readFile('/cat-locations.txt', function(err, content) {
+        console.log(content); // prints cat-locations.txt file!
+    });
 });
