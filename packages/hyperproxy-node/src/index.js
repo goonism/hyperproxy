1bc76727b3b84800b8586bb535e398fc904e3c28
Stefan Aleksic <stefan@stefanaleksic.com>
2018-04-07 16:29:01 -0400

    Add dat node functionality.

--- packages/hyperproxy-node/src/index.js
+++ packages/hyperproxy-node/src/index.js
@@ -1,20 +1,44 @@
 import Dat from 'dat-node';
 
-// 1. Tell Dat where to download the files
-Dat('./tmp/storage', {
-    // 2. Tell Dat what link I want
-    // (a 64 character hash from above)
-    key: '40a7f6b6147ae695bcbcff432f684c7bb5291ea339c28c1755896cdeb80bd2f9/',
-    temp: true,
-    sparse: true
-}, function(err, dat) {
-    if (err) {
-        throw err;
+//40a7f6b6147ae695bcbcff432f684c7bb5291ea339c28c1755896cdeb80bd2f9
+class HyperproxyNode {
+    constructor(channelName) {
+        this.datResolve = this._connectToDat(channelName);
+        // TODO implement swarm @coldsauce https://github.com/goonism/hyperproxy/issues/6
     }
-    // 3. Join the network & download (files are automatically downloaded)
-    dat.joinNetwork();
 
-    dat.archive.readFile('/index.html', function(err, content) {
-        // console.log(content.toString('utf8'));
-    });
-});
+    async readFile(fileName) {
+        await this.datResolve;
+        return new Promise((resolve, reject) => {
+            this.dat.archive.readFile(fileName, (err, content) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(content);
+            });
+        });
+    }
+
+    _connectToDat(key) {
+        const datConfig = {
+            key,
+            temp: true,
+            sparse: true
+        };
+        return new Promise((resolve, reject) => {
+            // 1. Tell Dat where to download the files
+            Dat(`./tmp/storage/${key}`, datConfig, (err, dat) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                // 3. Join the network & download (files are automatically downloaded)
+                dat.joinNetwork();
+                this.dat = dat;
+                resolve(dat);
+            });
+        });
+    }
+}
+
