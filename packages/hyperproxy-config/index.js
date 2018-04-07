f556ebe28b48fba04127f04c11fc477aabf8c1e1
Joe Johans <lgvichy@gmail.com>
2018-04-07 17:20:25 -0400

    Integrate client into browser

--- packages/hyperproxy-config/index.js
+++ packages/hyperproxy-config/index.js
@@ -1,6 +1,7 @@
-module.exports = {
+module.exports = Object.freeze({
+    HUB_NAME: 'hyperproxy',
     HUB_URL: 'wss://hyperproxy-hub-nsspddnqun.now.sh'
-};
+});
 
 
 // For local development of the hub
