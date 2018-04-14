a3710bbe559fcf32fb129b6437622f7f903894ed
Joe Johans <lgvichy@gmail.com>
2018-04-14 02:01:25 -0400

    Adding hyperproxy hub as a cli

--- packages/hyperproxy-hub/index.js
+++ packages/hyperproxy-hub/index.js
@@ -1,1 +1,14 @@
-module.exports = require('./lib');
+#!/usr/bin/env node
+
+'use strict';
+
+var currentNodeVersion = process.versions.node;
+var semver = currentNodeVersion.split('.');
+var major = semver[0];
+
+if (major < 4) {
+  console.error('NODE VERSION TOO OLD!');
+  process.exit(1);
+}
+
+require('./lib');
