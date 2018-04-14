c9f98d2b8d933b27c887aaeb5755a9d2eae73542
Joe Johans <lgvichy@gmail.com>
2018-04-14 03:06:13 -0400

    Make the config cleaner

--- packages/hyperproxy-config/index.js
+++ packages/hyperproxy-config/index.js
@@ -2,26 +2,22 @@
     Return Hub Variable
 */
 
+const HUB_NAME = 'hyperproxy';
+
 const HUB_IP = process.env.HUB_IP || 'localhost';
 const HUB_PORT = process.env.HUB_PORT || 9999;
 
-console.log(HUB_IP, HUB_PORT);
+const HUB_URL = process.env.LOCAL
+    ? `ws://${HUB_IP}:${PORT}`
+    : `ws://${HUB_IP}`;
 
-// Construct hub url based on environment
-function getHubURL() {
-    if (process.env.LOCAL) {
-        return `ws://${HUB_IP}:${PORT}`;
-    }
-    else {
-        return `ws://${HUB_IP}`;
-    }
-}
+console.log(HUB_URL);
 
 module.exports = Object.freeze({
+    HUB_NAME,
     HUB_IP,
     HUB_PORT,
-    HUB_NAME: 'hyperproxy',
-    HUB_URL: getHubURL(),
+    HUB_URL,
     HUB_MSG_TYPE: {
         JOIN: 'JOIN',
         REQUEST: 'REQUEST',
