b6978b12d3aef69b3b0ea9859b086506d900e182
Joe Johans <lgvichy@gmail.com>
2018-04-17 00:35:42 -0400

    remove local

--- packages/hyperproxy-config/index.js
+++ packages/hyperproxy-config/index.js
@@ -7,7 +7,7 @@ const HUB_NAME = 'hyperproxy';
 const HUB_IP = process.env.HUB_IP || 'localhost';
 const HUB_PORT = process.env.HUB_PORT || 9999;
 
-const HUB_URL = process.env.LOCAL = true
+const HUB_URL = process.env.LOCAL
     ? `ws://${HUB_IP}:${HUB_PORT}/`
     : `wss://hub.hyperproxy.network/`;
 
